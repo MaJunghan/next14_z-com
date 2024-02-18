f59cbd1c443257ff51bdf029f377fc52e51076f0
MaJungHan <wjdgksak@naver.com>
2024-02-18 23:28:55 +0900

    tab,postForm

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,14 @@
+import Post from '../_component/Post';
+import PostForm from './_component/PostForm';
+import Tab from './_component/Tab';
+import styles from './homoe.module.scss';
+
 export default function Home() {
-  return <div>홈페이지</div>;
+  return (
+    <main className={styles.main}>
+      <Tab />
+      <PostForm />
+      <Post />
+    </main>
+  );
 }
