0d5e8af89c2891e4fee4157abed9b9c1ad1b2b81
MaJungHan <wjdgksak@naver.com>
2024-03-03 21:54:00 +0900

    add : message page

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,3 +1,18 @@
+import style from './message.module.scss';
+import Room from '@/app/(afterLogin)/messages/_component/Room';
+
 export default function Message() {
-  return <div>message</div>;
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+    </main>
+  );
 }
