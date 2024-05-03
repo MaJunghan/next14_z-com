f0bde75d0de0beaf3ad56fc7c6293a70fb6642e1
MaJungHan <wjdgksak@naver.com>
2024-05-03 23:17:51 +0900

    add : next.js appRouter memo

---
+++ memo.js
@@ -0,0 +1,5 @@
+// Next.js 14 AppRouter
+
+// 1. 모든 렌더링은 서버사이드 렌더링 ('use client' 유무에 상관없이)
+// 2. 서버사이드 렌더링을 진행하게 되면 초기 HTML 반환
+// 3. Next 프레임워크가 초기화되면 HTML 위에 ReactApp 생성 (interactive) -> hydration
